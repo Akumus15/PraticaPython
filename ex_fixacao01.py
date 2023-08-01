d20a6916d0f1404b6de924a2322b7009c8359617
Pedro Mamed <mamed14.pedro@gmail.com.br>
2023-08-01 11:19:16 -0300

    segundo commit, com os exercícios feitos

---
+++ ex_fixacao01.py
@@ -0,0 +1,5 @@
+dolar = float(input("Qual o valor em dollar que você gostaria que eu convertese em reais ?"))
+
+result = dolar * 4.78
+
+print("O valor em real é:  ", result)
---
+++ ex_fixacao02.py
@@ -0,0 +1,3 @@
+gorjeta = float(input("Insira o valor da sua conta, para que possamos descobrir o acressimo de 10% do garçom"))
+total = gorjeta + ( gorjeta * 0.10)
+print("O valor total de sua conta será ", total)
