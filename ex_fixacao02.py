b80a58984df8d1845a1c7c941e0f03575fb88f00
Pedro Mamed <mamed14.pedro@gmail.com.br>
2023-08-01 11:23:05 -0300

    segundo commit, com os exercícios feitos terceira tentativa

--- ex_fixacao02.py
+++ ex_fixacao02.py
@@ -1,3 +1,3 @@
-conta = float(input("Insira o valor da sua conta, para que possamos descobrir o acressimo de 10% do garçom"))
-total = conta + ( conta * 0.10)
+gorjeta = float(input("Insira o valor da sua conta, para que possamos descobrir o acressimo de 10% do garçom"))
+total = gorjeta + ( gorjeta * 0.10)
 print("O valor total de sua conta será ", total)
